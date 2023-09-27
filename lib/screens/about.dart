5b84077dddb1fd2fb902b7b02ae6d3c801c9d35d
aratheunseen <62181222+aratheunseen@users.noreply.github.com>
2023-09-28 03:27:38 +0600

    last update

--- lib/screens/about.dart
+++ lib/screens/about.dart
@@ -41,9 +41,9 @@ class _AboutState extends State<About> {
             IconButton(
               onPressed: () async {
                 await launchOutside(Uri.parse(
-                    'https://play.google.com/store/apps/details?id=bornomala.eboardresults'));
+                    'https://github.com/BornomalaSoftware/eBoardResults_Bangladesh'));
                 FirebaseAnalytics.instance
-                    .logEvent(name: "rate_this_app_top_icon");
+                    .logEvent(name: "rate_this_app_top_github");
               },
               icon: const Icon(Icons.rate_review_rounded,
                   color: Color.fromARGB(255, 65, 57, 30), size: 24),
@@ -91,9 +91,9 @@ class _AboutState extends State<About> {
                   ),
                   onPressed: () async {
                     await launchOutside(Uri.parse(
-                        'https://play.google.com/store/apps/details?id=bornomala.eboardresults'));
+                        'https://github.com/BornomalaSoftware/eBoardResults_Bangladesh'));
                     FirebaseAnalytics.instance
-                        .logEvent(name: "rate_this_app_bottom_button");
+                        .logEvent(name: "rate_this_app_bottom_github");
                   },
                   child: const Text(
                     'Rate this application',
